7d36c69cd56a551dc201e963315c351856a55b97
kapibarasan-desu <72201255+kapibarasan-desu@users.noreply.github.com>
2021-06-04 16:57:20 +0900

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,2 +1,2 @@
-const API_KEY = AIzaSyDulz7tuaqsUaI-OY7KkTyK8RtapZbo20I;
-const SHEET_ID = 1_BnhdiqQqdRonBTCZPZiKV-iDaSsPpziE5wFAWzJsBc;
+const API_KEY = 'AIzaSyDulz7tuaqsUaI-OY7KkTyK8RtapZbo20I';
+const SHEET_ID = '1_BnhdiqQqdRonBTCZPZiKV-iDaSsPpziE5wFAWzJsBc';
